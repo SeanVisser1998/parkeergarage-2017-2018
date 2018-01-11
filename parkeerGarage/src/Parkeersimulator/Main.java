509130bb832a9f31a3d06b8c3b24ac0d706464cf
SeanVisser1998 <32290279+SeanVisser1998@users.noreply.github.com>
2018-01-11 17:29:27 +0100

    Eclipse ready
    Klaargemaakt om direct te importeert te worden in Eclipse.

---
+++ parkeerGarage/src/Parkeersimulator/Main.java
@@ -0,0 +1,19 @@
+package Parkeersimulator;
+/**
+ * 
+ * @author Sean Visser
+ *
+ */
+public class Main extends Simulator{
+	
+	/*
+	 * Fix voor Eclipse:
+	 * Run > Run Configurations > Java Application > Main > Parkeersimulator.Main
+	 */
+	
+	public static void main(String args[]) {
+		Simulator sim = new Simulator();
+		sim.run();
+	}
+
+}
